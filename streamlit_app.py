a64eddfeb51710a0208d30ed6a91d523cd83c24b
robertcfc <rorta@contfinco.com>
2025-11-11 19:23:51 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,39 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders")
+st.write("Orders that need to be filled.")
+
+# Get the current credentials
+session = get_active_session()
+
+my_dataframe = session.table("smoothies.public.orders").filter(col("order_filled")==0)
+
+rows = my_dataframe.collect()
+    
+
+if len(rows)== 0:
+    st.success("There are no pending orders right now.", icon=':material/thumb_up:')
+
+else:
+# if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset
+                 , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                 , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                )
+            st.success('Order(s) Updated!', icon=':material/thumb_up:')
+        except:
+            st.write('Something went wrong', icon=':material/thumb_down:')
+#else:
+    
